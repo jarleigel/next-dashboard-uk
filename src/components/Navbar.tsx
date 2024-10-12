2477082a88d73fda284a9a449c98d38d0bbf5942
jarleigel <jarleigel@gmail.com>
2024-10-12 16:25:09 +0000

    første

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+	return (
+		<div className='flex items-center justify-between p-4'>
+			{/* SEARCH BAR */}
+			{/* ICONS and USERS */}
+		</div>
+	)
+}
+
+export default Navbar
