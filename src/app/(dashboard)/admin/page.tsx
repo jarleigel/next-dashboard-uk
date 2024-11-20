9424348c511f4572fcfba2a1d859661cd7bea727
jarleigel <jarleigel@gmail.com>
2024-11-20 09:08:32 +0000

    FørPrisma

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,46 @@
+import Announcements from '@/components/Announcements'
+import AttendanceChart from '@/components/AttendanceChart'
+import CountChart from '@/components/CountChart'
+import EventCalendar from '@/components/EventCalendar'
+import FinanceChart from '@/components/FinanceChart'
+import UserCard from '@/components/UserCard'
 import React from 'react'
 
 const Admin = () => {
-	return <div>Admin</div>
+	return (
+		<div className='p-4 flex gap-4 flex-col md:flex-row'>
+			{/* LEFT */}
+			<div className='w-ful lg:w-2/3 flex flex-col gap-8'>
+				{/* USER CARD */}
+				<div className='flex gap-4 justify-between flex-wrap'>
+					<UserCard type='Student' />
+					<UserCard type='Teacher' />
+					<UserCard type='Parents' />
+					<UserCard type='Admin' />
+				</div>
+				{/* MIDDLE CHARTS */}
+				<div className='flex gap-4 flex-col lg:flex-row'>
+					{/* COUNT CHART */}
+					<div className='w-full lg:w-1/3 h-[450px]'>
+						<CountChart />
+					</div>
+					{/* ATTENDANCE CHART */}
+					<div className='w-full lg:w-2/3 h-[450px]'>
+						<AttendanceChart />
+					</div>
+				</div>
+				{/* BOTTOM CHARTS */}
+				<div className='w-full h-[500px]'>
+					<FinanceChart />
+				</div>
+			</div>
+			{/* RIGHT */}
+			<div className='w-full lg:w-1/3 flex flex-col gap-8'>
+				<EventCalendar />
+				<Announcements />
+			</div>
+		</div>
+	)
 }
 
 export default Admin
---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,169 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import InputField from '../InputField'
+import Image from 'next/image'
+
+const schema = z.object({
+	username: z
+		.string()
+		.min(3, { message: 'Brukernavnet må være minst 3 karakterer' })
+		.max(20, { message: 'Brukernavnet må være maks 20 karakterer' }),
+	email: z.string().email({ message: 'Invalid email address!' }),
+	password: z
+		.string()
+		.min(8, { message: 'Passordet må være minst 8 karakterer' }),
+	firstName: z.string().min(1, { message: 'Du må ha med fornavn' }),
+	lastName: z.string().min(1, { message: 'Du må ha med etternavn' }),
+	phone: z.string().min(1, { message: 'Du må ha med telefonnummer' }),
+	address: z.string().min(1, { message: 'Du må ha med adresse' }),
+	bloodType: z.string().min(1, { message: 'Du må ha med blod type' }),
+	birthday: z.date({ message: 'Du må ha med bursdag' }),
+	sex: z.enum(['male', 'female'], { message: 'Kjønn er påkrevd' }),
+	img: z.instanceof(File, { message: 'Bilde er påkrevd' }),
+})
+
+type Inputs = z.infer<typeof schema>
+
+const TeacherForm = ({
+	type,
+	data,
+}: {
+	type: 'create' | 'update'
+	data?: any
+}) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>({
+		resolver: zodResolver(schema),
+	})
+
+	const onSubmit = handleSubmit((data) => {
+		console.log(data)
+	})
+
+	return (
+		<form className='flex flex-col gap-8' onSubmit={onSubmit}>
+			<h1 className='text-xl font-semibold'>Create a new teacher</h1>
+			<span className='text-xs text-gray-400 font-medium'>
+				Authentication Information
+			</span>
+			<div className='flex justify-between flex-wrap gap-4'>
+				<InputField
+					label='Username'
+					name='username'
+					defaultValue={data?.username}
+					register={register}
+					error={errors.username}
+				/>
+				<InputField
+					label='Email'
+					name='email'
+					type='password'
+					defaultValue={data?.email}
+					register={register}
+					error={errors.email}
+				/>
+				<InputField
+					label='Password'
+					name='password'
+					defaultValue={data?.password}
+					register={register}
+					error={errors.password}
+				/>
+			</div>
+			<span className='text-xs text-gray-400 font-medium'>
+				Personal Information
+			</span>
+			<div className='flex justify-between flex-wrap gap-4'>
+				<InputField
+					label='First Name'
+					name='firstName'
+					defaultValue={data?.firstName}
+					register={register}
+					error={errors.firstName}
+				/>
+				<InputField
+					label='Last Name'
+					name='lastName'
+					defaultValue={data?.lastName}
+					register={register}
+					error={errors.lastName}
+				/>
+				<InputField
+					label='Phone'
+					name='phone'
+					defaultValue={data?.phone}
+					register={register}
+					error={errors.phone}
+				/>
+				<InputField
+					label='Address'
+					name='address'
+					defaultValue={data?.address}
+					register={register}
+					error={errors.address}
+				/>
+				<InputField
+					label='Blood Type'
+					name='bloodType'
+					defaultValue={data?.bloodType}
+					register={register}
+					error={errors.bloodType}
+				/>
+				<InputField
+					label='Bursdag'
+					name='birthday'
+					defaultValue={data?.birthday}
+					register={register}
+					error={errors.birthday}
+					type='date'
+				/>
+
+				<div className='flex flex-col gap-2 w-full md:w-1/4'>
+					<label className='text-xs text-gray-500'>Sex</label>
+					<select
+						className='ring-[1.5px] ring-gray-300 p-2 rounded-md  text-sm w-full'
+						{...register('sex')}
+						defaultValue={data?.sex}
+					>
+						<option value='male'>Male</option>
+						<option value='sfemale'>Female</option>
+					</select>
+					{errors.sex?.message && (
+						<p className='text-xs text-red-400'>
+							{errors.sex.message.toString()}
+						</p>
+					)}
+				</div>
+
+				<div className='flex flex-col gap-2 w-full md:w-1/4 justify-center'>
+					<label
+						className='text-xs text-gray-500 flex items-center gap-2 cursor-pointer'
+						htmlFor='img'
+					>
+						<Image src='/upload.png' alt='' width={28} height={28} />
+						<span>Upload a photo</span>
+					</label>
+					<input type='file' id='img' {...register('img')} className='hidden' />
+
+					{errors.img?.message && (
+						<p className='text-xs text-red-400'>
+							{errors.img.message.toString()}
+						</p>
+					)}
+				</div>
+			</div>
+
+			<button className='bg-blue-400 text-white p-2 rounded-md'>
+				{type === 'create' ? 'Create' : 'Update'}
+			</button>
+		</form>
+	)
+}
+
+export default TeacherForm
